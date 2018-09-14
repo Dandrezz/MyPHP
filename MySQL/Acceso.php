303c0a92cd944f139929d359fc5cb204d035dbfd
DIEGO ANDRES PORTERO LOPEZ <diego.portero@epn.edu.ec>
2018-09-13 22:16:42 -0500

    new file:   .gitignore 	new file:   JavaScript/index.html 	new file:   JavaScript/myscripts.js 	new file:   JavaScript/style.css 	modified:   MySQL/Acceso.php

--- MySQL/Acceso.php
+++ MySQL/Acceso.php
@@ -1,8 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=papeleria;charset=utf8', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=papeleria;charset=utf8','root', '');
 $sql = "SELECT idProducto, NombreP FROM producto";
 foreach ($pdo->query($sql) as $row) {
    echo $row['idProducto'] . " " . $row['NombreP'] . "<br />";
-   
 }
 ?>
